5767e0acd32175c76839c3d0b5b5ae8c17bca9d4
Yashchenko Victor <Andrey@Komp11.local>
2022-05-09 16:54:37 +0300

    still trying

--- src/Validator.js
+++ src/Validator.js
@@ -4,9 +4,9 @@ export default class Validator {
   }
 
   validateUsername(name) {
-    const first = /\d{0,3}/; //не более трех цифр подряд
-    const second = /^[^\d_-]$/; //не должен начинаться и заканчиваться цифрами, тире и подчеркиванием
-    const third = /^[a-z/dA-Z]+$/;
-	return first.test(name) && second.test(name) && third.test(name);
+    const first = /\d{0,4}/; //не более трех цифр подряд
+    const second = /^[^\d_-]+$/; //не должен начинаться и заканчиваться цифрами, тире и подчеркиванием
+    const third = /^[\w_-]+$/;
+	return !first.test(name) && second.test(name) && third.test(name);
   }
 }
